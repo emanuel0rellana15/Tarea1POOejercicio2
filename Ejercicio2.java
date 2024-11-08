4e9de432a49c96721444f0da8c0f66d74203516e
Emanuel Orellana <emanuel16orellana@gmail.com>
2024-11-07 20:50:40 -0600

    Tarea1 POO Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Operaciones Matematicas");
+        int x=50;
+        int y=9;
+        System.out.println("Numeros ingresados son " + x + " y " + y);
+        System.out.println("suma: " + (x+y));
+        System.out.println("resta: " + (x-y));
+        System.out.println("producto: " + (x*y));
+        System.out.println("Division: " + (x/y));
+        System.out.println("Mod: " + (x%y));
+    }
+}
